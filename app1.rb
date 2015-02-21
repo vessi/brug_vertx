16b3828a5c82425454427a148465d0e54d9fd575
Mikhail Bortnyk <vessimir@gmail.com>
2015-02-21 13:09:25 +0300

    Fixed port for vertx and switched default ruby to non-jruby

--- app1.rb
+++ app1.rb
@@ -7,6 +7,6 @@ server.request_handler do |request|
   request.response.put_header('Content-Type', 'application/json')
   hash = {status: 'ok'}
   request.response.end(hash.to_json)
-end.listen(3000) do |err|
+end.listen(9292) do |err|
   puts "Now listening!" if !err
 end
