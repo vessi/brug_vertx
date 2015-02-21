c39eecf6a8914bf3454fdb9c8ab5078f8bb97bb4
Mikhail Bortnyk <vessimir@gmail.com>
2015-02-21 12:48:24 +0300

    Initial commit with simpliest vertx application

---
+++ app1.rb
@@ -0,0 +1,12 @@
+require 'vertx'
+require 'json'
+
+server = Vertx::HttpServer.new
+
+server.request_handler do |request|
+  request.response.put_header('Content-Type', 'application/json')
+  hash = {status: 'ok'}
+  request.response.end(hash.to_json)
+end.listen(3000) do |err|
+  puts "Now listening!" if !err
+end
